1302ff0158156b3e37e5804337fff23df5434142
ja-ardila <ja.ardila2@uniandes.edu.co>
2024-10-28 19:27:16 -0500

    Cambios en config

--- config.py
+++ config.py
@@ -3,12 +3,14 @@ import os
 class Config:
     SECRET_TOKEN = 'my_static_token'  # Definición token estático
 
-    #SQLALCHEMY_DATABASE_URI = 'sqlite:///blacklist.db'
-
     # Configuración para la base de datos en AWS RDS
     SQLALCHEMY_DATABASE_URI = (
-         f'postgresql://{os.getenv("DB_USERNAME")}:{os.getenv("DB_PASSWORD")}@'
-         f'{os.getenv("DB_HOST")}/{os.getenv("DB_NAME")}'
+         f'postgresql://{os.getenv("DB_USERNAME", "")}:{os.getenv("DB_PASSWORD", "")}@'
+         f'{os.getenv("DB_HOST", "")}/{os.getenv("DB_NAME", "")}'
      )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     
+    if SQLALCHEMY_DATABASE_URI == "postgresql://:@/":
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///blacklist.db'
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
